922471261b20e381f9d46dc0d1aaeff357d5757b
etamarah <111328876+etamarah@users.noreply.github.com>
2023-02-11 13:00:21 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -32,4 +32,40 @@ module "vpc" {
 
   
   tags = {
+    Terrmodule "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = "my- 1st_vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = trueZZZZ
+  single_nat_gateway = true
+
+  create_database_subnet_group = true
+  create_database_subnet_route_table = true
+  database_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  public_subnet_tags = {
+    Type = "public-subnets"
+  }
+
+  private_subnet_tags = {
+    Type = "private-subnets"
+  }
+  database_subnet_tags = {
+    Type = "database-subnets"
+  }
+
+  
+  tags = {
     Terraform = "true"
+    Environment = "dev"
+    
